1e784e6ef848a8ff492c5b24c2dbf0f75bf81590
Manya Imran <i191760@nu.edu.pk>
2024-08-07 12:46:45 +0500

    final

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -7,11 +7,28 @@ const Socials = ({ className }) => {
   return (
     <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
       {yourData.socials.map((social, index) => (
-        <Button key={index} onClick={() => window.open(social.link)}>
+        <Button
+          key={index}
+          onClick={() => {
+            if (social.title === 'Email') {
+              window.open("mailto:manya.imran01@gmail.com");
+            } else {
+              window.open(social.link);
+            }
+          }}
+        >
           {social.title}
         </Button>
       ))}
     </div>
+
+    // <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
+    //   {yourData.socials.map((social, index) => (
+    //     <Button key={index} onClick={() => window.open(social.link)}>
+    //       {social.title}
+    //     </Button>
+    //   ))}
+    // </div>
   );
 };
 
